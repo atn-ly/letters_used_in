4642c84552146adac97aae4a27c1885f522561bf
Tam Ly <annatn.ly@mail.utoronto.ca>
2024-03-10 03:20:42 -0400

    Build logistic regression model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,41 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models political preference based on their gender and race.
+# Author: Renfrew Ao-Ieong, Rahma Binth Mohammad, Tam Ly
+# Date: 10 March 2024
+# Contact: renfrew.aoieong@mail.utoronto.ca, rahma.binthmohammad@mail.utoronto.ca, annatn.ly@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run the "02-data.cleaning.R" script.
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+ces2022_cleaned <- read_parquet("data/analysis_data/ces2022_cleaned.parquet")
 
 ### Model data ####
-first_model <-
+set.seed(853)
+
+# Randomly sample 1000 observations
+ces2022_reduced <-
+  ces2022_cleaned |> 
+  slice_sample(n = 1000)
+
+political_preferences <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    voted_for ~ gender + race,
+    data = ces2022_reduced,
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
-
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  political_preferences,
+  file = "models/political_preferences.rds"
 )
-
-
